a3a7028302c3f5c0e84a297f27899c7318f6cc5f
banjara9928 <108273237+banjara9928@users.noreply.github.com>
2022-07-17 13:15:33 +0530

    Add files via upload

---
+++ python_condition.py
@@ -0,0 +1,37 @@
+fname = 'Rohit'
+lname = 'Banjara'
+
+#if fname == 'Rohit' and lname == 'Banjara':
+ #   print('yes first name is Rohit')
+#else:
+       # print('No first name is not Rohit')
+
+
+        #if 'Rohit' in ['a', 'b', 'c']:
+         #   print('Rohit is in list')
+        #elif 'p' in 'Rohit':
+         #   print('Rohit has p in spelling.')
+        #elif 'B' in 'Banjara':
+        #    print('Banjara has B in spelling.')
+       # else:
+      #      print('Kuch hi nahi mila.')
+
+
+#some_string = input()
+#for i in some_string:
+   # print(i, ord(i))
+  #  if ord(i) % 2 == 0:
+ #       print(f' {i} is even of {ord (i)} position of ascii') 
+
+
+
+#no_of_inputs = input()
+#no_of_inputs = int(no_of_inputs)
+#lst = []
+
+#for i in range(no_of_inputs):
+ #   some_input = input('enter some input:')
+  #  lst.append(some_input)
+
+   # print(lst) 
+
